ab7bb08064d96b94cfde67f7719e05fa26e4ad5b
Kasturi Deepak <rkdeepakd@gmail.com>
2020-04-16 00:19:54 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Center(child: Text('Ask Me Anything',style: TextStyle(color: Colors.black),)),
+          ),
+          body: AskMe()
+        )
       ),
     );
+
+class AskMe extends StatefulWidget {
+
+  @override
+  _AskMeState createState() => _AskMeState();
+}
+
+class _AskMeState extends State<AskMe> {
+  var BtnClick = 3;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+              child:FlatButton(onPressed: (){
+                setState(() {
+                  BtnClick = Random().nextInt(5)+ 1;
+                  print('btn:$BtnClick');
+                });
+    },
+                   child: Image.asset('images/ball$BtnClick.png')),
+
+        ),
+    
+      ],
+    );
+  }
+}
